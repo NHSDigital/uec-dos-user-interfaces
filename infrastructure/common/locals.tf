27f0dc433062683fb56aeb5e4a07d85d02d00ddd
Tim Rickwood <tim.rickwood@nhs.net>
2023-08-25 09:09:13 +0100

    DR-357 Add perms to github iam policy (#3)
    * DR-357 Refactor repo name
    
    * DR-357 Remove dead code

---
+++ infrastructure/common/locals.tf
@@ -0,0 +1,6 @@
+# ==============================================================================
+# Context
+
+locals {
+  account_id = data.aws_caller_identity.current.id
+}
--- scripts/project-common.sh
+++ scripts/project-common.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 # Used by bootstrapper and infra-deploy scripts so defined once here
-export REPO_NAME=uec-dos-user-interfaces
+export TF_VAR_repo_name=uec-dos-user-interfaces
 export ACCOUNT_PROJECT=dos
